7102696e044036a84b82cb02c23077a3973ab87e
Chris King <kingces95@gmail.com>
2019-01-14 15:00:32 -1000

    XFStartUp app

---
+++ XFStartUp/XFStartUp.Android/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace XFStartUp.Droid
+{
+    [Activity(Label = "XFStartUp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+			XFStartUp.MainPage.Start = Java.Lang.JavaSystem.CurrentTimeMillis();
+
+			TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+
+		protected override void OnResume()
+		{
+			base.OnResume();
+			XFStartUp.MainPage.End = Java.Lang.JavaSystem.CurrentTimeMillis();
+		}
+	}
+}
